c5c9c6f1a4122d6a5d3fa5a8de8ba72d58b5d2ad
Jameson Stahl <jstahl@carthage.edu>
2021-02-05 14:09:21 -0600

    Changed square function to power function

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-void square(int x){
-    cout << (x * x) << endl;
+void power(int base, int exp){
+    cout << pow(base, exp) << endl;
 }
 
 int main(int argc, const char * argv[]) {
-    int x;
-    cout << "Enter a value to be squared: ";
-    cin >> x;
-    square(x);
+    int base;
+    int exp;
+    cout << "Enter a value to be raised to an exponent: ";
+    cin >> base;
+    cout << "Enter a value to be the exponent: ";
+    cin >> exp;
+    power(base, exp);
 
     return 0;
 }
